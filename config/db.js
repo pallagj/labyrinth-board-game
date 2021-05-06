5222f7d6fd93f7eb446199633a8db8ffc02a9827
Jonatán Pallag <>
2021-05-06 14:17:29 +0000

    MongoUrl

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(`mongodb+srv://labyrinth-board-game:${process.env.mongoPassword}@cluster0.aqsn9.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {useNewUrlParser: true}).then(r =>{})
+mongoose.connect(process.env.mongoUrl, {useNewUrlParser: true}).then(r =>{})
 
 
 module.exports = mongoose
